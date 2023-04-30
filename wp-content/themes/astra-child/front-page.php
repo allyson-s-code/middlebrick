91fbb922d0b453af8315e8af955a60696d431f35
allyson_s <allysonsmithstudio@gmail.com>
2023-04-29 21:03:26 -0400

    Front page as custom template iniated  and custom posts

--- wp-content/themes/astra-child/front-page.php
+++ wp-content/themes/astra-child/front-page.php
@@ -21,9 +21,55 @@ get_header(); ?>
 
 	<div id="primary" class="home-page hero-content"<?php astra_primary_class(); ?>>
 		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+
+			<div class="hero-container">
+				<h1 class="hero-title">Mosaics by Middlebrick</h1>
+			</div>	
+		
+			<div class="hero-images">
+				<?php query_posts('post_type=hero_image'); ?>
+				<?php while ( have_posts() ) : the_post(); 
+					$image = get_field("hero_image");
+					$size = "full";
+				?>
+				<figure class="hero-image">
+					<?php if ($image) {
+						echo wp_get_attachment_image( $image, $size );
+					} ?>
+				</figure>	
+				<?php endwhile; // end of the loop. ?>
+				<figure class="hero-image shop-entrance animate rotate">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Middlebrick-Shop-Entrance.png" alt="Enter Middlebrick shop graphic">
+				
+				</figure>
+			</div>
+
+			<div class="recent-builds-heading">
+				<h2>Recent Builds</h2>
+			</div>
+
+			<div class="recent-builds">
+				<?php query_posts('posts_per_page=2&post_type=recent_builds'); ?>
+				<?php while ( have_posts() ) : the_post(); 
+					$image = get_field("recent_build");
+					$size = "full";
+				?>
+				<figure class="recent-build">
+					<?php if ($image) {
+						echo wp_get_attachment_image( $image, $size );
+					} ?>
+				</figure>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</div>
+
+			<div class="shop-link-heading">
+				<h2 class="shop-link-h2">
+					<a href="<?php echo site_url("/shop/") ?>">Visit Shop for More</a>
+				</h2>
+			</div>
+
+			
 		</div><!-- .main-content -->
 		
 
--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -17,4 +17,38 @@ function astra_child_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'astra_child_scripts' );
 
-
+// Custom post types function
+function create_custom_post_types() {
+	//create a hero-image custom post type
+	register_post_type( 'hero_image',
+        array(
+            'labels' => array(
+                'name' => __( 'Hero Image' ),
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'hero-image' ),
+        )
+    );
+	register_post_type( 'recent_builds',
+        array(
+            'labels' => array(
+                'name' => __( 'Recent Builds' ),
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'recent-builds' ),
+        )
+    );
+	register_post_type( 'about',
+        array(
+            'labels' => array(
+                'name' => __( 'About' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'about' ),
+        )
+    );
+}
+add_action( 'init', 'create_custom_post_types' );
